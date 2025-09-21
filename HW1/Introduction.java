ec08fb09db56794aa70f68cc1858877a540bbb2a
Nguyen Minh Hung <minhhunglh06@gmail.com>
2025-09-21 11:19:45 +0700

    Homework 1: Introduction

---
+++ HW1/Introduction.java
@@ -0,0 +1,26 @@
+public class Introduction {
+    public static void main(String[] args) {
+        //Họ tên, mã sinh viên, lớp, username tài khoản github đã mở, địa chỉ email
+        System.out.println("Nguyễn Minh Hùng\t24020145\tK69I-IT1\tminhhung06lh\tminhhunglh06@gmail.com");
+        System.out.println(
+                "9 bottles of beer on the wall, 9 bottles of beer.\n" +
+                "Take one down, pass it around, \n" +
+                "8 bottles of beer on the wall, 8 bottles of beer.\n" +
+                "Take one down, pass it around,\n" +
+                "7 bottles of beer on the wall, 7 bottles of beer.\n" +
+                "Take one down, pass it around,\n" +
+                "6 bottles of beer on the wall, 6 bottles of beer.\n" +
+                "Take one down, pass it around,\n" +
+                "5 bottles of beer on the wall, 5 bottles of beer.\n" +
+                "Take one down, pass it around,\n" +
+                "4 bottles of beer on the wall, 4 bottles of beer.\n" +
+                "Take one down, pass it around,\n" +
+                "3 bottles of beer on the wall, 3 bottles of beer.\n" +
+                "Take one down, pass it around,\n" +
+                "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+                "Take one down, pass it around,\n" +
+                "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                "Take one down, pass it around,\n" +
+                "No more bottles of beer on the wall.\n");
+    }
+}
